f849b2ac0a49692ccc0c67e29d58f9f0aa2190c4
Sophie Deskin <sophiedeskin@gmail.com>
2021-09-23 10:03:35 -0700

    fixed server info formula

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 console.clear();
-const uuid = require('./helpers/uuid');
 const express = require('express');
 const fs = require('fs');
 const PORT = 3001;
 const app = express();
 const path = require("path")
+const uuid = require('./helpers/uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,49 +12,46 @@ app.use(express.static('public'));
 // WHEN I open the Note Taker
 // THEN I am presented with a landing page with a link to a notes page
 // WHEN I click on the link to the notes page
-app.get('/', (req, res) =>
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET request for notes
-app.get('/notes', (req, res) => {
-    // Send a message to the client
+app.get("/notes", (req, res) => 
     // res.json(`${req.method} request received to get notes`);
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-    // Log our request to the terminal
-    console.info(`${req.method} request received to get notes`);
-  });
-  app.get("/api/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "public/notes.html")))
+app.get("/api/notes", (req, res) => 
   res.sendFile(path.join(__dirname, "db/db.json"))
   )
   // POST request to add a note
-  app.post('api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
+  app.post('/api/notes', (req, res) => {
+    console.info(`POST request received to add a note`);
+    const {title, text} = req.body;
   
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
     if (title && text) {
-      // Variable for the object we will save
       const newNote = {
         title,
         text,
-        id: uuid(),
+        note_id: uuid(),
       };
   
-      // Convert the data to a string so we can save it
-      const noteString = JSON.stringify(newNote);
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+          parsedNotes.push(newNote);
   
-      // Write the string to a file
-      fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `Note for ${newNote.title} has been written to JSON file`
-            )
-      );
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+      });
   
       const response = {
         status: 'success',
@@ -71,4 +68,3 @@ app.get('/notes', (req, res) => {
   app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
   );
-  
