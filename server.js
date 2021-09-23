0bf9936473660ec04e67da8589c2d979b2c415ec
Sophie Deskin <sophiedeskin@gmail.com>
2021-09-22 17:23:35 -0700

    updating server info

--- server.js
+++ server.js
@@ -5,51 +5,70 @@ const fs = require('fs');
 const PORT = 3001;
 const app = express();
 const path = require("path")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // WHEN I open the Note Taker
 // THEN I am presented with a landing page with a link to a notes page
 // WHEN I click on the link to the notes page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-//Get request for notes
-app.get("/api/notes", (req, res) => {
-    res.json(`${req.method} request received to get notes`);
+// GET request for notes
+app.get('/notes', (req, res) => {
+    // Send a message to the client
+    // res.json(`${req.method} request received to get notes`);
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+    // Log our request to the terminal
     console.info(`${req.method} request received to get notes`);
-})
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-//post request to add note
-app.post("/api/notes", (req, res) => {
-console.info(`${req.method} request received to add note`)
-})
-
-//destructuring assignment for the items in req.body
-const { title, text } = req.body;
-
-if (title && text) {
-    const newNote = {
+  });
+  app.get("/api/notes", (req, res) => 
+  res.sendFile(path.join(__dirname, "db/db.json"))
+  )
+  // POST request to add a note
+  app.post('api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
         title,
         text,
         id: uuid(),
-    }
-    fs.readFile('.db/db.json', 'utf8', (err, data) => {
-        if(err) {
-            console.error(err);
-        } else {
-            const parsedNotes =  JSON.parse(data);
-            parsedNotes.push(newNote);
-            fs.writeFile(
-                './db/db.json',
-                JSON.stringify(parsedNotes, null, 4),
+      };
+  
+      // Convert the data to a string so we can save it
+      const noteString = JSON.stringify(newNote);
+  
+      // Write the string to a file
+      fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+              `Note for ${newNote.title} has been written to JSON file`
             )
-        }
-    })
-}
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+      );
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+  });
+  
+  app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+  );
+  
